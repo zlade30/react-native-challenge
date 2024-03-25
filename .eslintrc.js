41046c720223f04fe2ea0b4aa0b33431365325d3
zal <zal.dev.30@gmail.com>
2024-03-26 07:00:16 +0800

    Fix prettier eol error

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,7 @@ module.exports = {
     root: true,
     extends: '@react-native',
     rules: {
-        // other rules...
-        // You may need to override any conflicting rules
-        'prettier/prettier': ['error', {tabWidth: 4}],
+        'prettier/prettier': ['error', {tabWidth: 4, endOfLine: 'auto'}],
         'react-hooks/exhaustive-deps': 'off',
     },
 };
